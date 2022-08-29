8d2fcfea633edb9efcb475c2b64764b38e288629
LucasZuq <lucasdeozuquetto@hotmail.com>
2022-08-28 21:27:35 -0300

    post on sign-up and data validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 
 const server = express()
 
-server.listen(5000)
+server.use(cors())
+server.use(express.json())
+
+const users = []
+const tweets = []
+
+server.post('/sign-up', (req, res) => {
+    const {username, avatar} = req.body
+    if(!username || !avatar){
+        res.status(400).send('Todos os campos são obrigatórios!')
+        return
+    }
+    users.push(req.body)
+    res.status(200).send('OK')
+})
+
+server.listen(5000,() => console.log('listening on port 5000'))
